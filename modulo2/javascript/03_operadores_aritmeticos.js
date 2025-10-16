00ee34cefad8726a8eecfb07a40efab257b2f9cc
Melanie Estévez <melaniestevez@hotmail.com>
2025-10-16 09:57:27 -0500

    clase 2025-10-15

---
+++ modulo2/javascript/03_operadores_aritmeticos.js
@@ -0,0 +1,42 @@
+console.log("Operadores Aritmeticos");
+
+numero1=20;
+numero2=5;
+console.log("Suma");
+suma = numero1 + numero2;
+console.log("Resultado",suma);
+
+console.log("Resta");
+resta = numero1 - numero2;
+console.log("Resultado", resta);
+
+console.log("Multiplicacion");
+multiplicacion = numero1 * numero2;
+console.log("Resultado", multiplicacion);
+
+console.log("Division");
+division = numero1 / numero2;
+console.log("Resultado", division);
+
+console.log("Modulo");
+modulo = numero1%numero2;
+console.log("Resultado", modulo);
+
+console.log("Potencia");
+potencia = numero1**numero2;
+console.log("Resultado", potencia);
+
+console.log("Operadores Comparación");
+console.log("Igualdad debil ==", "5" ==5);
+console.log("Igualdad estricta ===", "5" ===5);
+console.log("Desigualdad debil !=", "5" != 5);
+console.log("Desigualdad estricta !==", "5" !== 5);
+console.log("Mayor que >", "5" >5);
+console.log("Menor que <", "5" <5);
+console.log("Mayor o igual  >=", "5">=5);
+console.log("Menor o igual  <=", "5"<=5);
+
+console.log("Operadores Lógica");
+console.log("y and &&", "5"<=5 && "5">=5);
+console.log("o Or ||", "5"<=5 || "6">=4);
+console.log("Negacion !", !(5==5));
