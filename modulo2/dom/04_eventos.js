06b354af8dea905a88c5233257b9f7aaac201d94
Melanie Estévez <melaniestevez@hotmail.com>
2025-10-28 09:58:42 -0500

    clase 2025-10-28

--- modulo2/dom/04_eventos.js
+++ modulo2/dom/04_eventos.js
@@ -7,3 +7,17 @@ document.getElementById('crear').addEventListener('click',()=>{
 document.getElementById('eliminar').addEventListener('click',()=>{
     if(parrafo) parrafo.remove();
 })
+
+function saludar(){
+    alert("Hola desde la funcion");
+}
+
+document.getElementById('parrafo')
+    .addEventListener('dbclick',()=>{
+    alert("Se hixo doble click en el parrafo")
+})
+
+document.getElementById('nombre')
+   .addEventListener('input',(e)=>{
+    console.log("Escribiendo", e.target.value)
+})
