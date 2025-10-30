dba28ac87da797755bd45cbe69cade9fb76aa055
Melanie Estévez <melaniestevez@hotmail.com>
2025-10-30 15:16:47 -0500

    clase 2025-10-30

---
+++ modulo2/typescript/07_persona.ts
@@ -0,0 +1,16 @@
+export class Persona {
+    nombre: string;
+    edad: number;
+    constructor( 
+        nombre: string,
+        edad: number
+    ){
+        this.nombre=nombre;
+        this.edad = edad;
+    }
+    saludar(): void{
+        console.log(
+        `Hola, soy ${this.nombre} y tengo ${this.edad} años`)
+    }
+
+}
---
+++ modulo2/typescript/08_persona.ts
@@ -0,0 +1,5 @@
+import {Persona} from './07_persona';
+
+const ana: Persona = new Persona('Ana', 27);
+
+ana.saludar();
